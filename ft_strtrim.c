7ca7f5d2aff024b08a261e135e171fde6e199857
root <root@DESKTOP-JDEMI8P.localdomain>
2025-01-13 15:31:49 +0100

    libft

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,10 +12,40 @@
 
 #include "libft.h"
 
+int	ft_inset(char c, const char *set)
+{
+	size_t	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (c == set[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*s2;
+	size_t	start;
+	size_t	end;
+	size_t	len;
 
 	if (!s1 || !set)
 		return (NULL);
+	start = 0;
+	while (s1[start] && ft_inset(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_inset(s1[end - 1], set))
+		end--;
+	len = end - start;
+	s2 = (char *)malloc(len + 1);
+	if (!s2)
+		return (NULL);
+	ft_memcpy(s2, s1 + start, len);
+	s2[len] = '\0';
+	return (s2);
 }
